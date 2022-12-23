d4ffa16839421b40da4b57bac913ce3c234ee427
casiohigh <118794180+casiohigh@users.noreply.github.com>
2022-12-23 18:19:56 +0800

    Update trex.py

--- trex.py
+++ trex.py
@@ -4,7 +4,8 @@ import numpy as np
 import mss
 from pynput.keyboard import Key, Controller
 
-keyboard = Controller()
+#контроллер для отправки событий виртуальной клавиатуры в систему
+keyboard = Controller() 
 
 with mss.mss() as sct:
     # Подбираем разрешение
@@ -14,17 +15,17 @@ with mss.mss() as sct:
     upperBound=np.array([0,0,83])
 
     while "Screen capturing":
-        last_time = time.time()
-        img = np.array(sct.grab(monitor))
-        imgHSV= cv2.cvtColor(img,cv2.COLOR_RGB2HSV)
-        mask=cv2.inRange(imgHSV,lowerBound,upperBound)
-        kernelOpen=np.ones((5,5))
+        last_time = time.time() #засекаю время
+        img = np.array(sct.grab(monitor)) #захват игры
+        imgHSV= cv2.cvtColor(img,cv2.COLOR_RGB2HSV) # перевод из ргб в хсв (лучше цветопередача)
+        mask=cv2.inRange(imgHSV,lowerBound,upperBound) # маска границы
+        kernelOpen=np.ones((5,5)) # массив из единиц размером 5 на 5
         kernelClose=np.ones((20,20))
-        maskOpen=cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernelOpen)
-        maskClose=cv2.morphologyEx(maskOpen,cv2.MORPH_CLOSE,kernelClose)
+        maskOpen=cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernelOpen) # закрывает просветы
+        maskClose=cv2.morphologyEx(maskOpen,cv2.MORPH_CLOSE,kernelClose) # убирает шум
 
-        _,conts,h=cv2.findContours(maskClose.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-        if(len(conts)>1):
+        conts,h=cv2.findContours(maskClose.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE) # берем контуры из копии закрывающей маски, чтобы не менялась основная маска
+        if(len(conts)>1): # если длинна контура превышает единицу
             keyboard.press(Key.space)
             keyboard.release(Key.space)
         else:
@@ -37,3 +38,5 @@ with mss.mss() as sct:
         if cv2.waitKey(25) & 0xFF == ord("q"):
             cv2.destroyAllWindows()
             break
+            
+            # ВАЖНО!!! ОКНО ДОЛЖНО СМОТРЕТЬ ПЕРЕД ДИНОЗАВРОМ
